fd846eb4c11d7791dd42afb3e00b9a3e5969da14
yeosiowvic <victoryeo1974@gmail.com>
2022-03-11 21:36:16 +0800

    add bond maker address

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,7 +1,10 @@
 const contracts = {
   bondTokenFactory: {
     4: '0x2dBbb0f06D255Fe9Ba4e64C42DD82448b547865b',
-  }
+  },
+  bondMaker: {
+    4: '0x3b1eAe73cA7558D8608eB255F95f47ef2be4008d',
+  },  
 }
 
 export default contracts
