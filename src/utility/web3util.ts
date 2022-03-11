14a1f7ed8534420d57dd54a341be01376c6b746c
yeosiowvic <victoryeo1974@gmail.com>
2022-03-11 21:01:02 +0800

    use ethers.Contract to get contract

--- src/utility/web3util.ts
+++ src/utility/web3util.ts
@@ -1,31 +1,38 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
+import { ethers } from 'ethers'
+import { Web3Provider } from '@ethersproject/providers'
+import BondTokenFactoryAbi from '../config/abi/BondTokenFactory.json'
 import Web3 from 'web3'
-import BondTokenFactoryAbi from 'config/abi/BondTokenFactory.json'
+import contracts from '../config/constants/contracts'
 
 const api_key = process.env.REACT_APP_API_KEY
 const rinkeby = `https://eth-rinkeby.alchemyapi.io/v2/${api_key}`
+const RINKEBY = 4
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [4],
+  supportedChainIds: [RINKEBY],   //rinkeby
 })
 
-export function getLibrary(provider:any) {
+export function getLibrary(provider:any): Web3Provider {
   console.log("getLibrary")
   console.log(provider)
-  return new Web3(provider)
+  //const library = new Web3(provider)
+  //return library
+
+  const library = new Web3Provider(provider)
+  library.pollingInterval = 12000
+  return library
 }
 
-export const getAddress = (address: string): string => {
-  const chainId = 4
-  return address[chainId]
-}
-
-const getContract = (abi: any, address: string) => {
-  const web3 = new Web3(new Web3.providers.HttpProvider(rinkeby));
-  const contract = new web3.eth.Contract(abi, address)
+const getContract = (abi: any, address: string, signer: ethers.Signer|ethers.providers.Provider) => {
+  //const web3 = new Web3(new Web3.providers.HttpProvider(rinkeby));
+  //const contract = new web3.eth.Contract(abi, address)
+  const contract = new ethers.Contract(address, abi, signer)
   return contract
 }
 
-export const getBondTokenFactoryContract = (address: string) => {
-  return getContract(BondTokenFactoryAbi, address)
+export const getBondTokenFactoryContract = (signer: ethers.Signer|ethers.providers.Provider) => {
+  console.log("getBondTokenFactoryContract")
+  console.log(signer)
+  return getContract(BondTokenFactoryAbi, contracts.bondTokenFactory[RINKEBY], signer)
 }
